7b66e3e35a16ca8e5ab356dfd5b5520ca24d1ab0
MHoussam <mohammadosta1999@gmail.com>
2023-07-19 10:57:41 +0300

    Updated the total amount ot change whenever adding a new expense or removing one.

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,14 @@
+let total_amount=0;
 
 function expenseItemElement(name, amount) {
-    return `<td>
-      <span class="text">${name}</span>  
-      <span class="number">${amount}</span>      
-      <button class="delete">&#128465</button>
-    </td>`
+    return `<tr>
+        <td>
+        <span class="text">${name}</span>  
+        <span class="number">${amount}</span>      
+        <button class="delete">&#128465</button>
+        </td>
+    </tr>`
   }
-  
   
   function addExpenseItem() {
     const expenseNameInput = $("#expense-name");
@@ -15,13 +17,22 @@ function expenseItemElement(name, amount) {
   
     if (expenseNameInput.val().trim() === "" || expenseAmountInput.val().trim() === "") return;
   
-    const expenseItem = $(expenseItemElement(expenseNameInput.val(), expenseAmountInput.val()))
+    const expenseItem = $(expenseItemElement(expenseNameInput.val(), parseFloat(expenseAmountInput.val()).toFixed(2)))
+    total_amount += parseFloat(expenseAmountInput.val());
   
-    //todo: remove
     expenseItem.find(".delete").click(function () {
+        const amount = expenseItem.find(".number");
+        total_amount -= parseFloat(amount.text());
         expenseItem.remove()
+        console.log(total_amount);
+        total.text(total_amount.toFixed(2))
+        console.log(total.val());
     })
-  
+
+    const total = $("#total");
+    total.text(total_amount.toFixed(2))
+    console.log(total.val());
+
     expenseTable.append(expenseItem)
     expenseNameInput.val("")
     expenseAmountInput.val("")
